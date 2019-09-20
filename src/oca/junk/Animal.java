8fbf3ca81e56a72be34371c2345843e9d6f7d6dd
RichardMarkRoss <cptrichkidprice@gmail.com>
2019-09-20 15:09:47 +0200

    new lacal date

--- src/oca/junk/Animal.java
+++ src/oca/junk/Animal.java
@@ -7,18 +7,22 @@ public class Animal {
         this.name = name;
     }
 
-    private Animal(){
+    Animal(){
         this(makeRandomName());
     }
 
-    private static String makeRandomName(){
+    static String makeRandomName(){
         int x = (int)(Math.random() * 5);
 
         return new String[]{"Fluffy", "Fido", "Rover", "Spike", "Gigi"}[x];
     }
-
+    static void makeRandomName(String name){
+        System.out.println(name);
+    }
     public static void main(String[] args) {
         Animal animal = new Animal();
         System.out.println(animal.name);
+        makeRandomName("Richard");
+
     }
 }
---
+++ src/oca/junk/TimeCon.java
@@ -0,0 +1,12 @@
+package oca.junk;
+
+import java.time.LocalDate;// using local date need to call time package
+import java.time.Month;// month is a enum packege to return the value of the month for date package
+
+public class TimeCon {
+        public static void main(String [] args) {
+            LocalDate date = LocalDate.of(2068, 4, 15);// instatiating new value cancels out values
+            date = LocalDate.of(2019, Month.DECEMBER,13);// call enum or values for 1 to 10...
+            System.out.println(date.getMonth() + ":" + date.getMonthValue());// getMonth gets interger value and getMonthValue gets the interger value
+        }
+}
