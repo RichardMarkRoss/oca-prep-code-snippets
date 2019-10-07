000d9e417d0d83a35906c139108c42c221f149da
RichardMarkRoss <cptrichkidprice@gmail.com>
2019-10-07 16:15:53 +0200

    new commit

---
+++ src/oca/arraylist/JIT.java
@@ -0,0 +1,13 @@
+package oca.arraylist;
+
+public class JIT {
+    void takesAnArray(int[] someArray) {
+
+    }
+    public static void main(String[] args){
+
+        JIT j = new JIT();
+        int[] values = new int[]{7, 7, 8, 2, 5};
+        j.takesAnArray(values);
+    }
+}
